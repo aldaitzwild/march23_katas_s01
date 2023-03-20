5ab24a877273c79c571149d6e0e3fc6e764f31be
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-03-20 09:48:53 +0100

    add the correction for monday

--- monday.php
+++ monday.php
@@ -21,3 +21,21 @@
  * guessWordCount("téléphone", 12) => 3
  * 
  */
+
+ function guessWordCount(string $word, int $length) : bool | int
+ {
+    $wordLength = strlen($word);
+
+    if($length === $wordLength)
+        return true;
+
+    return $length - $wordLength;
+ }
+
+ echo (guessWordCount("toto", 4) === true ? "c'est vrai !" : guessWordCount("toto", 4)) . PHP_EOL;
+ echo guessWordCount("carnaval", 8) . PHP_EOL;
+ echo guessWordCount("souris", 6) . PHP_EOL;
+
+ echo guessWordCount("titi", 6) . PHP_EOL;
+ echo guessWordCount("tartine", 4) . PHP_EOL;
+ echo guessWordCount("telephone", 12) . PHP_EOL;
