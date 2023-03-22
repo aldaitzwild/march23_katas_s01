e84a57f4a58c6c6a5e8a7e75db3e484bb7511c71
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-03-22 08:52:18 +0100

    add a new kata for wednesday

---
+++ wednesday.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * - Ecrire une fonction square() qui prend un chiffre en entrée et qui renvoi 
+ * ce même chiffre multiplié par lui-même
+ * 
+ * 
+ * - Ecrire une fonction SelectForSquare() qui prend deux chiffres en entrée, 
+ *  appelle la fonction square() sur le plus grand des deux et retourne le resultat
+ * 
+ * - Ecrire une fonction qui génére deux chiffres aléatoirement et 
+ *  appelle SelectForSquare() avec, puis affiche le resultat.
+ */
