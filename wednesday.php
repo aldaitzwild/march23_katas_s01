1623a65d9951f7e0b841c28f775dc895e804169e
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-03-22 09:28:33 +0100

    fix few things in the summary

--- wednesday.php
+++ wednesday.php
@@ -4,9 +4,9 @@
  * ce même chiffre multiplié par lui-même
  * 
  * 
- * - Ecrire une fonction SelectForSquare() qui prend deux chiffres en entrée, 
+ * - Ecrire une fonction selectForSquare() qui prend deux chiffres en entrée, 
  *  appeller la fonction square() sur le plus grand des deux et retourner le resultat
  * 
  * - Ecrire une fonction qui génére deux chiffres aléatoirement et 
- *  appelle SelectForSquare() avec, puis afficher le resultat.
+ *  appelle selectForSquare() avec, puis afficher le resultat.
  */
