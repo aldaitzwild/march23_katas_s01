b091f55b27110f78e96071a0c01e985bfac723b1
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-03-22 09:50:42 +0100

    Add the correction for Wednesday

--- wednesday.php
+++ wednesday.php
@@ -10,3 +10,25 @@
  * - Ecrire une fonction qui génére deux chiffres aléatoirement et 
  *  appelle selectForSquare() avec, puis afficher le resultat.
  */
+
+ function square(int $value): int 
+ {
+    return $value * $value;
+ }
+
+ function selectForSquare(int $firstNumber, int $secondNumber): int
+ {
+    return square(max($firstNumber, $secondNumber));
+ }
+
+ function randSquare() : void
+ {
+    $firstNumber = rand(0, 100);
+    $secondNumber = rand(0, 100);
+
+    echo "Valeur 1 : $firstNumber\n";
+    echo "Valeur 2 : $secondNumber\n";
+    echo 'Le carré généré est  ' . selectForSquare($firstNumber, $secondNumber);
+ }
+
+ randSquare();
